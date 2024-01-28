8b14d5d9cbf9cc1f89b052f8a926543386132cf8
Adiy03 <adiyogo3@gmail.com>
2024-01-28 09:48:17 +0700

    get_user

--- auth.ts
+++ auth.ts
@@ -6,16 +6,6 @@ import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
  
-async function getUser(email: string): Promise<User | undefined> {
-  try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0];
-  } catch (error) {
-    console.error('Failed to fetch user:', error);
-    throw new Error('Failed to fetch user.');
-  }
-}
- 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
@@ -41,4 +31,12 @@ export const { auth, signIn, signOut } = NextAuth({
   ],
 });
 
-
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
+  }
+}
